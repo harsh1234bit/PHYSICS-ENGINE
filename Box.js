45ca7764dca197001667259bfa67dd6afbdcab16
harsh1234bit <74101952+harsh1234bit@users.noreply.github.com>
2020-12-02 19:05:00 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,28 @@
+class Box {
+
+constructor (x,y,width,height  ){
+
+    var options={
+        restitution: 3.0
+      }
+this.body = Bodies.rectangle(x,y,width,height,options);
+World.add(world,this.body);
+this.width=width
+this.height=height
+}
+display(){
+  var pos = this.body.position
+  rectMode(CENTER)  
+  fill (255)
+  rect(pos.x,pos.y,this.width,this.height)
+  var angle = this.body.angle
+  push()
+ translate(pos.x,pos.y)
+ rotate(angle) 
+ rectMode(CENTER)
+ fill(255)
+ rect (0,0,this.width,this.height)
+ pop ();
+
+}
+} 
